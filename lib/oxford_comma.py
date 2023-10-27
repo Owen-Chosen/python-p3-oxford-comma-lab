eb914c38eec4bb2617145b79db73469404d041f8
owen <owen.chosen@gmail.com>
2023-10-27 18:42:49 +0100

    Done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if(len(items)>2):
+        items[-1] = 'and '+ items[-1]
+        return ', '.join(items)
+    elif(len(items)==1):
+        return ''.join(items)
+    else:
+        items[-1] = 'and '+ items[-1]
+        return ' '.join(items)
+
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))
